18d0ee70a4dee3eaa69c99948278b0000d3f2ea5
plromanens <Pierre-Louis.Romanens@studentfr.ch>
2023-11-08 14:21:35 +0100

    j'ai coder l'effet d'opacité de case-0

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const logo = document.querySelector(".case-0-logo-tibet");
+// const logo = document.querySelector(".case-0-logo-tibet");
+// const caseDeux = document.querySelector(".case-0-logo-tibet");
 
-gsap.to(logo, {
+gsap.to("#js-logo", {
   scrollTrigger: {
-    trigger: logo,
-    start: "100px 20%",
-    end: "bottom 100px",
-    markers: true,
+    trigger: "#js-logo",
+    start: "top top",
+    end: "bottom+=200px 70%",
+    // markers: true,
+    scrub: true,
+    id: "logo",
     // toggleActions: "start",
   },
-  y: "500px",
-  duration: 2,
+  opacity: 0,
+  duration: 5,
+});
+
+gsap.to(caseDeux, {
+  scrollTrigger: {
+    trigger: caseDeux,
+    start: "top top",
+    end: "bottom+=200px 70%",
+    // markers: true,
+    scrub: true,
+    id: "case2",
+  },
+  opacity: 100,
+  duration: 5,
 });
