3c79e23302d2f3f41663ea0b58642c4d89ec398f
plromanens <Pierre-Louis.Romanens@studentfr.ch>
2023-12-10 18:05:00 +0100

    j'ai améliorer l'animation de la case 2

--- src/js/main.js
+++ src/js/main.js
@@ -39,20 +39,26 @@ gsap.to("#js-background-mountain", {
 // --------------------------------------
 // Case-2 Moine
 // --------------------------------------
-
-gsap.from("#js-case-2-moine-1", {
+const tl3 = gsap.timeline({
   scrollTrigger: {
-    trigger: "#js-case-2-moine-1",
-    start: "top 90%",
-    end: "bottom+=100px 100%",
-    // markers: true,
+    trigger: ".main-case-2",
+    start: "top+=100px top+=100px",
+    end: "bottom top",
+    pin: true,
     scrub: true,
+    markers: true,
     id: "case2",
   },
+});
+
+tl3.to("#js-case-2-moine-1", {
   opacity: 0,
   duration: 5,
 });
-
+tl3.from("#js-case-2-moine-1", {
+  opacity: 0,
+  duration: 5,
+});
 gsap.to("#js-case-2-moine-2", {
   y: -100,
   duration: 2,
@@ -60,18 +66,31 @@ gsap.to("#js-case-2-moine-2", {
   yoyo: true,
   ease: "power1.inOut",
 });
-
 // gsap.from("#js-case-2-moine-1", {
 //   scrollTrigger: {
 //     trigger: "#js-case-2-moine-1",
-//     start: "top 65%",
-//     end: "bottom+=100px 100%",
-//     // markers: true,
+//     start: "top+=-300px center",
+//     end: "top+=-100px 10%",
 //     scrub: true,
+
+//     // markers: true,
 //     id: "case2",
 //   },
 //   opacity: 0,
-//   duration: 5,
+//   duration: 20,
+// });
+
+// gsap.to("#js-case-2-moine-1", {
+//   scrollTrigger: {
+//     trigger: "#js-case-2-moine-1",
+//     start: "top 25%",
+//     end: "bottom 50%",
+//     scrub: true,
+//     // markers: true,
+//     id: "case2-out",
+//   },
+//   opacity: 0,
+//   duration: 10,
 // });
 
 // gsap.to("#js-case-2-moine-2", {
@@ -91,7 +110,7 @@ const tl = gsap.timeline({
     start: "center center",
     // start: "top+=-250px top",
     end: "bottom+=180px top",
-    markers: true,
+    // markers: true,
     pin: true,
     scrub: true,
     // id: "case2à3",
@@ -109,22 +128,85 @@ tl.from("#js-case-3-general", {
   ease: "power1.inOut",
 });
 
+gsap.to("#js-case-3-moine", {
+  y: -100,
+  duration: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+});
+
 // --------------------------------------
 // Case-5 avalanche
 // --------------------------------------
 
-// gsap.from("#js-case-5-avalanche", {
+gsap.from("#js-case-5-avalanche", {
+  scrollTrigger: {
+    trigger: ".main-case-5",
+    start: "50px 65%",
+    end: "bottom+=100px 100%",
+    // markers: true,
+    scrub: true,
+    id: "avalanche",
+  },
+  x: -100,
+  y: -100,
+  duration: 5,
+  // yoyo: true,
+  ease: "power1.inOut",
+});
+
+// --------------------------------------
+// Case-11 jumelle
+// --------------------------------------
+const jumelleImage = document.getElementById("js-case-11-jumelle");
+const timeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: jumelleImage,
+    start: "top bottom",
+    end: "bottom+=-300px center",
+    scrub: true,
+    // markers: true,
+    id: "jumelle",
+  },
+});
+
+// Ajoutez l'animation de flou à netteté à la timeline
+timeline.from(jumelleImage, {
+  duration: 1,
+  filter: "blur(10px)",
+  ease: "power2.inOut",
+});
+// gsap.from("#js-case-11-jumelle", {
 //   scrollTrigger: {
-//     trigger: ".main-case-5",
-//     start: "50px 65%",
-//     end: "bottom+=100px 100%",
-//     // markers: true,
+//     trigger: "#js-case-11-jumelle",
+//     start: "top center",
+//     end: "bottom 10%",
 //     scrub: true,
-//     id: "avalanche",
+
+//     markers: true,
+//     id: "case-11",
 //   },
-//   x: -100,
-//   y: -100,
-//   duration: 5,
-//   // yoyo: true,
-//   ease: "power1.inOut",
+//   filter: "blur(3)",
+//   ease: "power2.inOut",
+//   duration: 3,
 // });
+
+// --------------------------------------
+// Case-15 tintin et milou
+// --------------------------------------
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".main-case-15",
+    start: "top+=-100px -10%",
+    end: "bottom top",
+    pin: true,
+    scrub: true,
+    // markers: true,
+    id: "tintinMilou",
+  },
+});
+
+tl2.to("#js-case-15-tintin-milou", {
+  scale: 1.5,
+});
