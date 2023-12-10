fee694fe53ea16c9eefd0966a5852ebbe72741f4
plromanens <Pierre-Louis.Romanens@studentfr.ch>
2023-12-10 10:04:44 +0100

    j'ai transformer toute mes images en webp

--- src/js/main.js
+++ src/js/main.js
@@ -6,104 +6,125 @@ gsap.registerPlugin(ScrollTrigger);
 const logo = document.querySelector(".case-0-logo-tibet");
 const caseDeux = document.querySelector(".case-0-logo-tibet");
 
-// // --------------------------------------
-// // Header
-// // --------------------------------------
+// --------------------------------------
+// Header
+// --------------------------------------
 
-// gsap.to("#js-logo-tibet", {
+gsap.to("#js-logo-tibet", {
+  scrollTrigger: {
+    trigger: "#js-logo-tibet",
+    start: "top top",
+    end: "bottom+=200px 70%",
+    scrub: true,
+    // markers: true,
+    id: "logo",
+    // toggleActions: "start",
+  },
+  opacity: 0,
+});
+
+gsap.to("#js-background-mountain", {
+  scrollTrigger: {
+    trigger: "#js-logo-tibet",
+    start: "top top",
+    end: "bottom+=300px 70%",
+    // markers: true,
+    scrub: true,
+    id: "header-background",
+    // toggleActions: "start",
+  },
+  opacity: 0,
+});
+
+// --------------------------------------
+// Case-2 Moine
+// --------------------------------------
+
+gsap.from("#js-case-2-moine-1", {
+  scrollTrigger: {
+    trigger: "#js-case-2-moine-1",
+    start: "top 90%",
+    end: "bottom+=100px 100%",
+    // markers: true,
+    scrub: true,
+    id: "case2",
+  },
+  opacity: 0,
+  duration: 5,
+});
+
+gsap.to("#js-case-2-moine-2", {
+  y: -100,
+  duration: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+});
+
+// gsap.from("#js-case-2-moine-1", {
 //   scrollTrigger: {
-//     trigger: "#js-logo-tibet",
-//     start: "top top",
-//     end: "bottom+=200px 70%",
-//     scrub: true,
+//     trigger: "#js-case-2-moine-1",
+//     start: "top 65%",
+//     end: "bottom+=100px 100%",
 //     // markers: true,
-//     id: "logo",
-//     // toggleActions: "start",
-//   },
-//   opacity: 0,
-// });
-
-// gsap.to("#js-background-mountain", {
-//   scrollTrigger: {
-//     trigger: "#js-logo-tibet",
-//     start: "top top",
-//     end: "bottom+=300px 70%",
-//     // markers: true,
 //     scrub: true,
-//     id: "header-background",
-//     // toggleActions: "start",
+//     id: "case2",
 //   },
 //   opacity: 0,
+//   duration: 5,
 // });
 
-// // --------------------------------------
-// // Case-2 Moine
-// // --------------------------------------
-
-// // gsap.from("#js-case-2-moine-1", {
-// //   scrollTrigger: {
-// //     trigger: "#js-case-2-moine-1",
-// //     start: "top 65%",
-// //     end: "bottom+=100px 100%",
-// //     // markers: true,
-// //     scrub: true,
-// //     id: "case2",
-// //   },
-// //   opacity: 0,
-// //   duration: 5,
-// // });
-
-// // gsap.to("#js-case-2-moine-2", {
-// //   y: -100,
-// //   duration: 2,
-// //   repeat: -1,
-// //   yoyo: true,
-// //   ease: "power1.inOut",
-// // });
-
-// // --------------------------------------
-// // Case-3 Moine2
-// // --------------------------------------
-// const tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".main-case-2",
-//     start: "center center",
-//     // start: "top+=-250px top",
-//     end: "bottom+=180px top",
-//     markers: true,
-//     pin: true,
-//     scrub: true,
-//     // id: "case2à3",
-//   },
-// });
-
-// tl.to("#js-case-2-moine-1", {
-//   opacity: 0,
-//   duration: 10,
-//   ease: "power1.inOut",
-// });
-// tl.from("#js-case-3-general", {
-//   opacity: 0,
-//   duration: 1,
+// gsap.to("#js-case-2-moine-2", {
+//   y: -100,
+//   duration: 2,
+//   repeat: -1,
+//   yoyo: true,
 //   ease: "power1.inOut",
 // });
 
-// // --------------------------------------
-// // Case-5 avalanche
-// // --------------------------------------
+// --------------------------------------
+// Case-3 Moine2
+// --------------------------------------
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".main-case-2",
+    start: "center center",
+    // start: "top+=-250px top",
+    end: "bottom+=180px top",
+    markers: true,
+    pin: true,
+    scrub: true,
+    // id: "case2à3",
+  },
+});
 
-// // gsap.from("#js-case-5-avalanche", {
-// //   scrollTrigger: {
-// //     trigger: ".main-case-5",
-// //     start: "50px 65%",
-// //     end: "bottom+=100px 100%",
-// //     // markers: true,
-// //     scrub: true,
-// //     id: "avalanche",
-// //   },
-// //   x: -100,
-// //   y: -100,
-// //   duration: 5,
-// //   // yoyo: true,
-// //   ease: "power1.inOut",
-// // });
+tl.to("#js-case-2-moine-1", {
+  opacity: 0,
+  duration: 10,
+  ease: "power1.inOut",
+});
+tl.from("#js-case-3-general", {
+  opacity: 0,
+  duration: 1,
+  ease: "power1.inOut",
+});
+
+// --------------------------------------
+// Case-5 avalanche
+// --------------------------------------
+
+// gsap.from("#js-case-5-avalanche", {
+//   scrollTrigger: {
+//     trigger: ".main-case-5",
+//     start: "50px 65%",
+//     end: "bottom+=100px 100%",
+//     // markers: true,
+//     scrub: true,
+//     id: "avalanche",
+//   },
+//   x: -100,
+//   y: -100,
+//   duration: 5,
+//   // yoyo: true,
+//   ease: "power1.inOut",
+// });
